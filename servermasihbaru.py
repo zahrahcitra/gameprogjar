3df7b78f9e0a7c63f5a487f164c75e25c8c88ba4
zahrahcitra <zahrahcitra@yahoo.co.id>
2016-12-19 16:27:28 +0700

    udah bisa nunggu pemain

--- servermasihbaru.py
+++ servermasihbaru.py
@@ -2,10 +2,19 @@ import PodSixNet.Channel
 import PodSixNet.Server
 from time import sleep
 
-class ClientChannel(PodSixNet.Channel.Channel):
 
+class ClientChannel(PodSixNet.Channel.Channel):
     def Network(self, data):
         print data
+
+    def Network_place(self, data):
+        self.x = data["horisontal"]
+        self.y = data["vertical"]
+        num = data["num"]
+        self.gameid = data["gameid"]
+        print data
+        gamenya(x,y)
+
 
 
 class BoxesServer(PodSixNet.Server.Server):
@@ -19,6 +28,34 @@ class BoxesServer(PodSixNet.Server.Server):
 
     def Connected(self, channel, addr):
         print 'new connection:', channel
+        if self.queue == None:
+            self.currentIndex += 1
+            channel.gameid = self.currentIndex
+            self.queue = gamenya(channel, self.currentIndex)
+        else:
+            channel.gameid = self.currentIndex
+            self.queue.player1 = channel
+            self.queue.player0.Send(
+                {"action": "startgame", "player": 0, "gameid": self.queue.gameid})
+            self.queue.player1.Send(
+                {"action": "startgame", "player": 1, "gameid": self.queue.gameid})
+            self.games.append(self.queue)
+            self.queue = None
+        # print "as Player ", self.playerke
+        # # playerid = self.playerke , channel
+        # # self.send({"action":"player", "playerke": self.playerke})
+        # self.playerke+=1
+
+class gamenya:
+    def __init__(self,player0, currentIndex):
+        self.player0 = player0
+        self.player1 = None
+        self.gameid = currentIndex
+
+
+
+    # def sending(self,x,y,player):
+
 
 
 host, port = "localhost", 6969
